a6cd7d66d698cf09dadb6a856ae62d24f94ac93e
7jajohnson <jajohnson7@miners.utep.edu>
2023-09-21 10:06:25 -0600

    finished

--- code_4.py
+++ code_4.py
@@ -3,16 +3,49 @@ import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 
 def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+    df = pd.DataFrame(pd.read_csv(big_mac_file))
+    # locate the country_code
+    df = df.loc[df['iso_a3'].str.lower()== country_code.lower()]
+    df = df.loc[df['date'].str[0:4] == year]
+    mean_price = 0
+    x = 0
+    for item in df['dollar_price']:
+        mean_price += item
+        x+=1
+    mean_price = mean_price / x
+    return(round(mean_price,2))
+
+
+
 
 def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    df = pd.DataFrame(pd.read_csv(big_mac_file))
+    # locate the country_code
+    df = df.loc[df['iso_a3'].str.lower() == country_code.lower()]
+    mean_price = 0
+    x = 0
+    for item in df['dollar_price']:
+        mean_price += item
+        x += 1
+    mean_price = mean_price / x
+    return (round(mean_price, 2))
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    df = pd.DataFrame(pd.read_csv(big_mac_file))
+    df = df.loc[df['date'].str[0:4] == year]
+    df = df.loc[df['dollar_price']==df['dollar_price'].min()]
+    print(f"{df['name'].values[0]}({df['iso_a3'].values[0]}) ${round(float(df['dollar_price'].values[0]),2)}")
+    return(f"{df['name'].values[0]}({df['iso_a3'].values[0]}) ${round(float(df['dollar_price'].values[0]),2)}")
 
+
+    # "country_name(country_code): $dollar_price"
 def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    df = pd.DataFrame(pd.read_csv(big_mac_file))
+    df = df.loc[df['date'].str[0:4] == year]
+    df = df.loc[df['dollar_price']==df['dollar_price'].max()]
+    print(f"{df['name'].values[0]}({df['iso_a3'].values[0]}) ${round(float(df['dollar_price'].values[0]),2)}")
+    return(f"{df['name'].values[0]}({df['iso_a3'].values[0]}) ${round(float(df['dollar_price'].values[0]),2)}")
+
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    get_the_most_expensive_big_mac_price_by_year("2008")
